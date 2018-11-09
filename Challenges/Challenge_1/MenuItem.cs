32057f9abd173e86f9ec8869304eb0e1def44047
Stephen Center <stephenbcenter@gmail.com>
2018-11-09 14:07:40 -0500

    first commit

---
+++ Challenges/Challenge_1/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1 {
+
+    class MenuItem {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public List<string> Ingredients { get; set; }
+
+        public MenuItem(string name, string description, int price, List<string> ingredients) {
+            Name = name;
+            Description = description;
+            Price = price;
+            Ingredients = ingredients;
+
+        }
+
+    }
+
+}
---
+++ Challenges/Challenge_1/ProgramUI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1 {
+
+    class ProgramUI {
+        MethodRepository mrep = new MethodRepository();
+        private const string divider = "-------------------------";
+
+        public void Run() {
+            Console.WriteLine("Welcome to Komodo Cafe's patented Menu Management System!");
+            ChooseAnOption();
+        }
+
+        public string Input(string prompt) {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public void ChooseAnOption() {
+            while (true) {
+                Console.WriteLine(divider);
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("      [1] Add a menu item");
+                Console.WriteLine("      [2] Remove a menu item");
+                Console.WriteLine("      [3] View the full menu");
+                Console.WriteLine("      [4] Exit program");
+
+                while (true) {
+                    string choice = Input("Input [#]: ");
+
+                    if (choice == "1") {
+                        AddOption();
+                    }
+
+                    if (choice == "2") {
+                        RemoveOption();
+                    }
+
+                    if (choice == "3") {
+                        ViewOption();
+                    }
+
+                    if (choice == "4") {
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+        }
+
+        public void AddOption() {
+
+        }
+
+        public void RemoveOption() {
+
+        }
+
+        public void ViewOption() {
+
+        }
+    }
+}
